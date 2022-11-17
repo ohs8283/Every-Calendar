82be497d31025e3eea1694c8d9a209760e4196d4
Hoseong On <ohs8283@naver.com>
2022-11-17 18:19:27 +0900

    (update)

--- js/signup.js
+++ js/signup.js
@@ -1,49 +1,25 @@
 const inputUserId = document.getElementById('inputId');
-const inputUserNickname = document.getElementById('inputNickname');
-const inputUserEmail = document.getElementById('inputEmail');
 const inputPassword = document.getElementById('inputPassword');
 const confrimPassword = document.getElementById('confirmPassword');
+const inputUserName = document.getElementById('inputName');
+const inputUserNickname = document.getElementById('inputNickname');
 
-inputUserId.addEventListener('keyup', IdCheck);
-inputUserNickname.addEventListener('keyup', NicknameCheck);
-inputUserEmail.addEventListener('keyup', emailCheck);
+inputUserId.addEventListener('keyup', idCheck);
 inputPassword.addEventListener('keyup', passwordCheck);
 confrimPassword.addEventListener('keyup', isSameCheck);
+inputUserName.addEventListener('keyup', nameCheck);
+inputUserNickname.addEventListener('keyup', nicknameCheck);
 
-function IdCheck() {
+function idCheck() {
     let userIdLength = inputUserId.value.length;
-    if (userIdLength >= 2 && userIdLength <= 16) {
-        document.querySelector('.validator.Id').innerHTML = `유효합니다.`;
+    if (userIdLength >= 8 && userIdLength <= 16) {
+        document.querySelector('.validator.userId').innerHTML = `유효합니다.`;
         document.querySelector('.userId').style.color = `#00C040`;
     } else {
-        document.querySelector('.validator.Id').innerHTML = `유효하지않습니다.`;
+        document.querySelector('.validator.userId').innerHTML = `아이디는 8자리이상 이어야합니다.`;
         document.querySelector('.userId').style.color = `rgba(233 50 35)`;
     }
 }
-
-function NicknameCheck() {
-    let userNicknameLength = inputUserNickname.value.length;
-    if (userNicknameLength >= 2 && userNicknameLength <= 16) {
-        document.querySelector('.validator.Nickname').innerHTML = `유효합니다.`;
-        document.querySelector('.userNickname').style.color = `#00C040`;
-    } else {
-        document.querySelector('.validator.Nickname').innerHTML = `유효하지않습니다.`;
-        document.querySelector('.userNickname').style.color = `rgba(233 50 35)`;
-    }
-}
-
-function emailCheck() {
-    const regEmail = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/;
-    if (regEmail.test(inputUserEmail.value) === true) {
-        document.querySelector('.validator.userEmail').innerHTML = `유효합니다.`;
-        document.querySelector('.userEmail').style.color = `#00C040`;
-    } else {
-        document.querySelector('.validator.userEmail').innerHTML = `이메일 형식이 올바르지 않습니다.`;
-        document.querySelector('.userEmail').style.color = `rgba(233 50 35)`;
-    }
-}
-
-
 function passwordCheck() {
     const regPassword = /^(?=.*?[0-9a-zA-Z])(?=.*?[#?!@$%^&*-]).{8,}$/;
     if (regPassword.test(inputPassword.value) === true) {
@@ -69,4 +45,37 @@ function isSameCheck() {
 }
 
 
+function nameCheck() {
+    const regName = /^[가-힣]{2,4}$/;
+        if (regName.test(inputUserName.value) === true) {
+            document.querySelector('.validator.userName').innerHTML = '유효합니다.';
+            document.querySelector('.userName').style.color = `#00C040`;
+        } else {
+            document.querySelector('.validator.userName').innerHTML = `올바르지 않습니다.`;
+            document.querySelector('.userName').style.color = `rgba(233 50 35)`;
+        }
+}
 
+
+function nicknameCheck() {
+    let userNicknameLength = inputUserNickname.value.length;
+    if (userNicknameLength >= 2 && userNicknameLength <= 16) {
+        document.querySelector('.validator.userNickname').innerHTML = `유효합니다.`;
+        document.querySelector('.userNickname').style.color = `#00C040`;
+    } else {
+        document.querySelector('.validator.userNickname').innerHTML = `유효하지않습니다.`;
+        document.querySelector('.userNickname').style.color = `rgba(233 50 35)`;
+    }
+}
+
+
+
+// 이메일 함수
+// const regEmail = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/;
+//     if (regEmail.test(inputUserEmail.value) === true) {
+//         document.querySelector('.validator.userEmail').innerHTML = `유효합니다.`;
+//         document.querySelector('.userEmail').style.color = `#00C040`;
+//     } else {
+//         document.querySelector('.validator.userEmail').innerHTML = `이메일 형식이 올바르지 않습니다.`;
+//         document.querySelector('.userEmail').style.color = `rgba(233 50 35)`;
+//     }
