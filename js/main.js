d91f5db267255dc569c590b2e9439ac66ab76a96
Hoseong On <ohs8283@naver.com>
2022-12-05 05:30:37 +0900

    <up date 12/5 캘린더 구인게시판>

--- js/main.js
+++ js/main.js
@@ -251,3 +251,26 @@ window.onload = function () {
   }
   
   }
+
+  
+  /* 사이드바 */
+function openSlideMenu() {
+  document.getElementById("menu").style.width = "260px";
+  document.getElementById("page").style.marginRight = "260px";
+}
+function closeSlideMenu() {
+  document.getElementById("menu").style.width = "0";
+  document.getElementById("page").style.marginRight = "0";
+}
+
+$("#logoutBtn").click(function () {
+  sessionStorage.removeItem("name");
+  sessionStorage.removeItem("email");
+  location.href = "../index.html";
+  sessionStorage.removeItem("postId");
+  sessionStorage.removeItem("user");
+});
+
+// 사이드바 끝
+
+$(".homeUserName").html(sessionStorage.getItem("name")); //~님
